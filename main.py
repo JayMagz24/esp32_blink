bac0996c96f20ed6c18d779e8973fcdb85d0af9d
JayMagz24 <41783491+JayMagz24@users.noreply.github.com>
2026-05-10 18:44:43 +0800

    Fix indentation in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from machine import Pin
-   from time import sleep
+from time import sleep
 
-   led = Pin(23, Pin.OUT)
+led = Pin(23, Pin.OUT)
 
-   while True:
-       led.value(not led.value())
-       sleep(0.5)
+while True:
+    led.value(not led.value())
+    sleep(0.5)
